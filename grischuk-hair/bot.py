f968145988a1d4c0fae1517e900b0e9328fa9494
grischuk4hair <grischuk.hair@gmail.com>
2025-07-13 15:15:16 +0200

    Update bot.py

--- grischuk-hair/bot.py
+++ grischuk-hair/bot.py
@@ -86,7 +86,7 @@ async def run_bot():
     await application.updater.start_polling()
 
 async def run_api():
-    config = uvicorn.Config(app, host="0.0.0.0", port=port, log_level="info")
+    config = uvicorn.Config(app, host="0.0.0.0", port=PORT, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
 
