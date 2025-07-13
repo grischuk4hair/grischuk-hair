05827f25f9ded853e4be48b25da555195aa16e3c
grischuk4hair <grischuk.hair@gmail.com>
2025-07-13 02:17:54 +0200

    Update start.sh

--- grischuk-hair/start.sh
+++ grischuk-hair/start.sh
@@ -4,3 +4,4 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 python3 bot.py
+curl -X POST "https://api.telegram.org/bot<7220866591:AAHXN0i2tYfhmCPUQKC-oGZv1ceBAkWmnds>/deleteWebhook" -d "drop_pending_updates=true"
